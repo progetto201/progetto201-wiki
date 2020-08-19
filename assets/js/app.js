a85df8ff1a5bcce0654b4de4544379a51338b619
Stefano Zenaro <mariortgasd@hotmail.com>
2020-08-19 21:45:24 +0200

    fix: toc, super small screens. notify updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,27 @@
-// verifica se il browser supporta le PWA
-if ('serviceWorker' in navigator) {
-    // registra il service worker
-    navigator.serviceWorker.register('/sw.js')
-    .then((reg) => {
-        console.log("Service worker registered", reg);
-    })
-    .catch((err) => {
-        console.log("Service worker not registered", err);
-    })
-}
+window.isUpdateAvailable = new Promise(function(resolve, reject) {
+    // verifica se il browser supporta le PWA
+    if ('serviceWorker' in navigator) {
+        // registra il service worker
+        navigator.serviceWorker.register('/sw.js')
+        .then(reg => {
+            /** Permetti di rilevare un nuovo aggiornamento per poi mostrare la notifica */
+            reg.onupdatefound = () => {
+                const installingWorker = reg.installing;
+                installingWorker.onstatechange = () => {
+                    switch (installingWorker.state) {
+                        case 'installed':
+                            if (navigator.serviceWorker.controller) {
+                                // new update available
+                                resolve(true);
+                            } else {
+                                // no update available
+                                resolve(false);
+                            }
+                            break;
+                    }
+                };
+            };
+        })
+        .catch(err => console.error('[SW ERROR]', err));
+    }
+});
